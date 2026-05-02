57001e77dc7d7a7d5b7776d9c26ef2c73d476f8b
COELHO DO AMARAL <soraya.coelho@t-systems.com>
2026-05-02 18:18:51 +0200

    create header

--- my-bullet-journal/src/App.jsx
+++ my-bullet-journal/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import logo from './images/Designer.png'
 import './App.css'
+
+
 
 
 function App() {
   return (
     <div>
+      <header>
       <h1>Weekly Journal Soraya</h1>
+      <img src={logo} className='logo' alt="logo"></img>
+      </header>
     </div>
   );
 }
