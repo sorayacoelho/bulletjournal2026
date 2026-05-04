3967387b2b355a320d9836f1e4cf28ed2d276348
COELHO DO AMARAL <soraya.coelho@t-systems.com>
2026-05-04 15:42:58 +0200

    commit

---
+++ my-bullet-journal/src/components/Calendar.jsx
@@ -0,0 +1,10 @@
+const Calendar = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1 className="header_h1">Soraya's Bullet Journal</h1>
+    </header>
+  )
+}
+
+export default Calendar
